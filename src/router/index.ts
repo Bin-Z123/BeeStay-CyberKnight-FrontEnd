f96f5e277a8dee5b8c3c86b50759c21f00e97d03
Bin-Z123 <binvtps40171@gmail.com>
2025-06-07 21:22:41 +0700

    fix link

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
-import authRouter from "@/router/routers/AuthRouter";
+import authRouter from "../router/routers/AuthRouter";
 import administrationRouter from "../router/routers/AdministrationRouter";
 const routes: RouteRecordRaw[] = [authRouter, administrationRouter];
  
--- src/router/routers/AuthRouter.ts
+++ src/router/routers/AuthRouter.ts
@@ -3,17 +3,17 @@ import { RouteRecordRaw } from "vue-router";
 const authRouter: RouteRecordRaw = {
     path: '/auth',
     name: 'auth',
-    component: () => import('@/layouts/auth/AuthLayout.vue'),
+    component: () => import('../../layouts/auth/AuthLayout.vue'),
     children: [
         {
             path: 'login',
             name: 'login',
-            component: () => import('@/pages/auth/LoginPage.vue')
+            component: () => import('../../pages/auth/LoginPage.vue')
         },
         {
             path: 'register',
             name: 'register',
-            component: () => import('@/pages/auth/RegisterPages.vue')
+            component: () => import('../../pages/auth/RegisterPages.vue')
         }
     ]
     
