32b1127dd76acc7e3f6c689a55cac176958a31e0
Bin-Z123 <binvtps40171@gmail.com>
2025-06-10 16:31:26 +0700

    tách dialog

---
+++ src/components/administration/roomTypeDialog/CreateRoomTypeDialog.vue
@@ -0,0 +1,79 @@
+<template>
+  <Dialog :open="open" @update:open="emit('update:open', $event)">
+    <DialogContent class="sm:max-w-[425px]">
+      <form action="" class="flex flex-col gap-3">
+        <DialogHeader>
+          <DialogTitle class="font-bold text-muesli-400"
+            >Thêm Loại Phòng Mới</DialogTitle
+          >
+          <hr class="text-muesli-400 bg-muesli-400 h-[2px]" />
+        </DialogHeader>
+        <div>
+          <label class="text-muesli-400">Loại Phòng</label><br />
+          <input
+            type="text"
+            class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
+            placeholder="Nhập loại phòng"
+          />
+        </div>
+        <div>
+          <label class="text-muesli-400">Diện Tích</label><br />
+          <input
+            type="text"
+            class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
+            placeholder="Nhập diện tích"
+          />
+        </div>
+        <div>
+          <label class="text-muesli-400">Giá</label><br />
+          <input
+            type="text"
+            class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
+            placeholder="Nhập giá"
+          />
+        </div>
+        <div>
+          <label class="text-muesli-400">Số Lượng Người Ở</label><br />
+          <input
+            type="text"
+            class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
+            placeholder="Nhập số lượng người ở"
+          />
+        </div>
+        <div>
+          <label class="text-muesli-400">Khuyến mãi</label><br />
+          <input
+            type="text"
+            class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
+            placeholder="Nhập giá"
+          />
+        </div>
+        <DialogFooter>
+          <Button
+            type="submit"
+            class="bg-muesli-400 hover:bg-muesli-600 text-white"
+          >
+            Lưu
+          </Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  </Dialog>
+</template>
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+const emit = defineEmits<{
+  (e: "update:open", value: boolean): void;
+}>();
+defineProps<{
+  open: boolean;
+}>();
+</script>
