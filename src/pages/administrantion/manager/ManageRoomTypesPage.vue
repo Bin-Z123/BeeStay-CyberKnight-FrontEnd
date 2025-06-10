8c99cb6fc264f62793ff76d87e160c54c0e8360c
Kiet <kietkid1st@gmail.com>
2025-06-10 10:52:58 +0700

    ManagerRoomTypesPage

--- src/pages/administrantion/manager/ManageRoomTypesPage.vue
+++ src/pages/administrantion/manager/ManageRoomTypesPage.vue
@@ -1,3 +1,71 @@
 <template>
-  <h1>Manage Room Types</h1>
+  <section>
+    <div class="bg-white">
+      <div class="flex relative">
+        <div class="w-1/2">
+          <div class="flex gap-2 items-center">
+            <input type="text"
+              class="w-2/6 h-10 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300 my-3 ms-4 text-center"
+              placeholder="Tìm kiếm">
+            <div class="relative w-2/6">
+              <select name="" id=""
+                class="appearance-none w-full h-10 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300 my-3 text-center">
+                <option value="">VIP</option>
+                <option value="">Thường</option>
+              </select>
+              <div class="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+                <ChevronDown class="w-5 h-5 text-gray-400" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-1/2 flex justify-end px-4">
+          <button @click="isAddRoomTypes = !isAddRoomTypes"
+            class="text-muesli-400 border px-4 my-3 shadow-sm shadow-muesli-300 rounded-lg font-medium hover:bg-muesli-300 hover:text-white hover:border-muesli-300">Thêm</button>
+          <div v-if="isAddRoomTypes" class="absolute -mt-5 bg-red-500 left-0 h-40 w-40">
+            <input type="text"
+              class="w-2/6 h-10 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300 my-3 ms-4 text-center">
+          </div>
+        </div>
+      </div>
+      <div class="shadow-lg px-4 pb-4">
+        <table class="w-full border border-gray-300 text-sm text-center bg-white h-132">
+          <thead class="bg-gradient-to-r from-muesli-200 to-muesli-400 text-white">
+            <tr>
+              <th class="px-4 py-2 border">ID</th>
+              <th class="px-4 py-2 border">Loại Phòng</th>
+              <th class="px-4 py-2 border">Diện Tích</th>
+              <th class="px-4 py-2 border">Giá</th>
+              <th class="px-4 py-2 border">Lượng Người Ở</th>
+              <th class="px-4 py-2 border">Khuyến Mãi</th>
+              <th class="px-4 py-2 border">Tùy Chọn</th>
+            </tr>
+          </thead>
+          <tbody class="text-gray-700">
+            <tr class="hover:bg-muesli-100 transition odd:bg-white even:bg-gray-100">
+              <td class="py-2">P01</td>
+              <td class="py-2">Thường</td>
+              <td class="py-2">50m²</td>
+              <td class="py-2">3000000</td>
+              <td class="py-2">3</td>
+              <td class="py-2">30%</td>
+              <td class="py-2 flex justify-center items-center gap-8 h-full"><button><LockKeyhole /></button><button><SquarePen /></button></td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="bg-white h-15 mb-4 shadow-lg flex items-center justify-end gap-2 px-5">
+          <input type="text" class="w-12 h-8 border border-gray-300 rounded-sm text-center" disabled value="1"><span>of 16</span>
+          <button class="hover:bg-muesli-100 w-10 h-10 flex items-center justify-center rounded-4xl"><ChevronLeft /></button>
+          <button class="hover:bg-muesli-100 w-10 h-10 flex items-center justify-center rounded-4xl"><ChevronRight /></button>
+        </div>
+      </div>
+
+    </div>
+  </section>
 </template>
+<script setup>
+import { ChevronDown, SquarePen, LockKeyhole, ChevronLeft, ChevronRight } from 'lucide-vue-next';
+import { ref } from 'vue';
+
+const isAddRoomTypes = ref(false)
+</script>
