dfc5b8782d12c2c9ad913454dc269cfafa17b4ce
Kiet <kietkid1st@gmail.com>
2025-06-10 21:56:13 +0700

    Booking Page

--- src/pages/administrantion/manager/ManageRoomTypesPage.vue
+++ src/pages/administrantion/manager/ManageRoomTypesPage.vue
@@ -6,7 +6,7 @@
           <div class="flex gap-2 items-center">
             <input type="text"
               class="w-2/6 h-10 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300 my-3 ms-4 text-center"
-              placeholder="Tìm kiếm">
+              placeholder="Tìm kiếm" />
             <div class="relative w-2/6">
               <select name="" id=""
                 class="appearance-none w-full h-10 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300 my-3 text-center">
@@ -19,58 +19,14 @@
             </div>
           </div>
         </div>
+
         <div class="w-1/2 flex justify-end px-4">
-          <Dialog>
-            <DialogTrigger as-child>
-              <Button
-                class="bg-white text-muesli-400 border border-muesli-400 hover:bg-muesli-400 hover:text-white px-4 my-3">
-                Thêm
-              </Button>
-            </DialogTrigger>
-            <DialogContent class="sm:max-w-[425px]">
-              <form action="" class="flex flex-col gap-3">
-                <DialogHeader>
-                  <DialogTitle class="font-bold text-muesli-400">Thêm Loại Phòng Mới</DialogTitle>
-                  <hr class="text-muesli-400 bg-muesli-400 h-[2px]">
-                </DialogHeader>
-                <div>
-                  <label class="text-muesli-400">Loại Phòng</label><br>
-                  <input type="text"
-                    class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                    placeholder="Nhập loại phòng">
-                </div>
-                <div>
-                  <label class="text-muesli-400">Diện Tích</label><br>
-                  <input type="text"
-                    class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                    placeholder="Nhập diện tích">
-                </div>
-                <div>
-                  <label class="text-muesli-400">Giá</label><br>
-                  <input type="text"
-                    class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                    placeholder="Nhập giá">
-                </div>
-                <div>
-                  <label class="text-muesli-400">Số Lượng Người Ở</label><br>
-                  <input type="text"
-                    class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                    placeholder="Nhập số lượng người ở">
-                </div>
-                <div>
-                  <label class="text-muesli-400">Khuyến mãi</label><br>
-                  <input type="text"
-                    class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                    placeholder="Nhập giá">
-                </div>
-                <DialogFooter>
-                  <Button type="submit" class="bg-muesli-400 hover:bg-muesli-600 text-white">
-                    Lưu
-                  </Button>
-                </DialogFooter>
-              </form>
-            </DialogContent>
-          </Dialog>
+          <!-- Btn bật dialog -->
+          <Button @click="isOpen = true"
+            class="bg-white text-muesli-400 border border-muesli-400 hover:bg-muesli-400 hover:text-white px-4 my-3">
+            Thêm
+          </Button>
+          <DialogCreateRoomType v-model:open="isOpen"></DialogCreateRoomType>
         </div>
       </div>
       <div class="shadow-lg px-4 pb-4 h-[622px]">
@@ -110,37 +66,37 @@
                     <form action="" class="flex flex-col gap-3">
                       <DialogHeader>
                         <DialogTitle class="font-bold text-muesli-400">Cập Nhật Loại Phòng Mới</DialogTitle>
-                        <hr class="text-muesli-400 bg-muesli-400 h-[2px]">
+                        <hr class="text-muesli-400 bg-muesli-400 h-[2px]" />
                       </DialogHeader>
                       <div>
-                        <label class="text-muesli-400">Loại Phòng</label><br>
+                        <label class="text-muesli-400">Loại Phòng</label><br />
                         <input type="text"
                           class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                          placeholder="Nhập loại phòng">
+                          placeholder="Nhập loại phòng" />
                       </div>
                       <div>
-                        <label class="text-muesli-400">Diện Tích</label><br>
+                        <label class="text-muesli-400">Diện Tích</label><br />
                         <input type="text"
                           class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                          placeholder="Nhập diện tích">
+                          placeholder="Nhập diện tích" />
                       </div>
                       <div>
-                        <label class="text-muesli-400">Giá</label><br>
+                        <label class="text-muesli-400">Giá</label><br />
                         <input type="text"
                           class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                          placeholder="Nhập giá">
+                          placeholder="Nhập giá" />
                       </div>
                       <div>
-                        <label class="text-muesli-400">Số Lượng Người Ở</label><br>
+                        <label class="text-muesli-400">Số Lượng Người Ở</label><br />
                         <input type="text"
                           class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                          placeholder="Nhập số lượng người ở">
+                          placeholder="Nhập số lượng người ở" />
                       </div>
                       <div>
-                        <label class="text-muesli-400">Khuyến mãi</label><br>
+                        <label class="text-muesli-400">Khuyến mãi</label><br />
                         <input type="text"
                           class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                          placeholder="Nhập giá">
+                          placeholder="Nhập giá" />
                       </div>
                       <DialogFooter>
                         <Button type="submit" class="bg-muesli-400 hover:bg-muesli-600 text-white">
@@ -155,8 +111,8 @@
           </tbody>
         </table>
         <div class="bg-white h-15 mb-4 shadow-lg flex items-center justify-end gap-2 px-5">
-          <input type="text" class="w-12 h-8 border border-gray-300 rounded-sm text-center" disabled value="1"><span>of
-            16</span>
+          <input type="text" class="w-12 h-8 border border-gray-300 rounded-sm text-center" disabled
+            value="1" /><span>of 16</span>
           <button class="hover:bg-muesli-100 w-10 h-10 flex items-center justify-center rounded-4xl">
             <ChevronLeft />
           </button>
@@ -169,20 +125,18 @@
   </section>
 </template>
 <script setup lang="ts">
-import { ChevronDown, SquarePen, LockKeyhole, ChevronLeft, ChevronRight } from 'lucide-vue-next';
-import { ref } from 'vue';
-import { Button } from '@/components/ui/button'
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
+  ChevronDown,
+  SquarePen,
+  LockKeyhole,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-vue-next";
+import { ref } from "vue";
+import { Button } from "@/components/ui/button";
+import DialogCreateRoomType from "@/components/administration/roomTypeDialog/CreateRoomTypeDialog.vue";
 // import { Input } from '@/components/ui/input'
 // import { Label } from '@/components/ui/label'
-
-const isAddRoomTypes = ref(false)
+const isOpen = ref(false);
+const isAddRoomTypes = ref(false);
 </script>
--- src/pages/administrantion/receptionist/BookingsPage.vue
+++ src/pages/administrantion/receptionist/BookingsPage.vue
@@ -1,3 +1,75 @@
 <template>
-  <h1>Bookings</h1>
+  <section>
+    <div class="flex">
+      <div class="w-1/2">
+        <div class="flex gap-2 items-center">
+          <input type="date"
+            class="w-2/6 h-10 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300 my-3 ms-4 text-center">
+          <label class="text-muesli-400 mx-4 font-medium">Đến</label>
+          <input type="date"
+            class="w-2/6 h-10 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300 my-3 text-center">
+          <div class="relative w-2/6">
+            <select name="" id=""
+              class="appearance-none w-full h-10 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300 my-3 text-center">
+              <option value="">VIP</option>
+              <option value="">Thường</option>
+            </select>
+            <div class="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+              <ChevronDown class="w-5 h-5 text-gray-400" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-1/2">
+        <div class="h-15 mb-4 flex items-center justify-end gap-2 px-5">
+          <input type="text" class="w-12 h-8 border border-gray-300 rounded-sm text-center" disabled
+            value="1" /><span>of 16</span>
+          <button class="hover:bg-muesli-100 w-10 h-10 flex items-center justify-center rounded-4xl">
+            <ChevronLeft />
+          </button>
+          <button class="hover:bg-muesli-100 w-10 h-10 flex items-center justify-center rounded-4xl">
+            <ChevronRight />
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center gap-2">
+      <div
+        class="w-35 h-40 bg-muesli-200 [clip-path:polygon(0%_25%,50%_0%,100%_25%,100%_75%,50%_100%,0%_75%)] flex items-center justify-center">
+        <h1 class="text-muesli-800 font-bold text-2xl">Tầng 1</h1>
+      </div>
+
+      <div class="grid grid-cols-7 gap-2 max-w-[1050px] justify-center relative">
+        <div v-for="(i, index) in 14" :key="i" class="w-[140px]" :class="{ '-ml-[74px] -mt-10': index >= 7 }">
+          <div @click="isOpenBooking = true"
+            class="h-40 bg-white hover:bg-gray-200 flex justify-center items-center [clip-path:polygon(0%_25%,50%_0%,100%_25%,100%_75%,50%_100%,0%_75%)]">
+            <div class="flex flex-col items-center p-4">
+              <h1 class="text-muesli-400 font-bold text-lg">T10{{ i }}</h1>
+              <h1>VIP</h1>
+              <h1>2 người</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <BookingDialog v-model:open="isOpenBooking" />
+    </div>
+
+  </section>
 </template>
+<script setup lang="ts">
+import {
+  ChevronDown,
+  SquarePen,
+  LockKeyhole,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-vue-next";
+import { ref } from "vue";
+import BookingDialog from "@/components/administration/roomTypeDialog/BookingDialog.vue";
+import { Button } from "@/components/ui/button";
+
+const isOpenBooking = ref(false);
+</script>
