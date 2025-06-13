23eedc4efb457d68971e84a1d0b562de431a86ee
Kiet <kietkid1st@gmail.com>
2025-06-13 14:19:01 +0700

    Crud Rank

--- src/pages/administrantion/manager/ManagerPromotionPage.vue
+++ src/pages/administrantion/manager/ManagerPromotionPage.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <Tabs default-value="voucher" class="bg-white">
+        <Tabs default-value="voucher" class="">
             <div>
                 <TabsList class="grid grid-cols-3 mx-4 mt-2 bg-muesli-300 rounded-b-none shadow-sm">
                     <TabsTrigger value="voucher"
@@ -113,49 +113,48 @@
                     <div class="w-1/3 me-4  mb-4 rounded-lg p-4">
                         <form action="" class="flex flex-col gap-3">
                             <div>
-                                <label class="text-muesli-400">Mã Giảm Giá</label><br />
+                                <label class="text-muesli-400">Hạng</label><br />
                                 <input type="text"
+                                    v-model="rank.nameRank"
                                     class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                                    placeholder="Nhập mã giảm giá" />
+                                    placeholder="Nhập tên hạng" />
                             </div>
                             <div>
-                                <label class="text-muesli-400">Giá Trị</label><br />
-                                <input type="text"
+                                <label class="text-muesli-400">Điểm Hạng</label><br />
+                                <input type="number"
+                                    v-model="rank.minPointRequired"
                                     class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
-                                    placeholder="Nhập giá trị" />
+                                    placeholder="Nhập điểm hạng" />
                             </div>
                             <div>
-                                <label class="text-muesli-400">Trạng Thái</label><br />
-                                <div class="relative">
-                                    <select name="" id=""
-                                        class="appearance-none w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center">
-                                        <option value="">1</option>
-                                        <option value="">2</option>
-                                        <option value="">3</option>
-                                    </select>
-                                    <div class="absolute inset-y-0 right-3 flex items-center pointer-events-none">
-                                        <ChevronDown class="w-5 h-5 text-gray-400" />
-                                    </div>
-                                </div>
+                                <label class="text-muesli-400">Giá Trị Giảm</label><br />
+                                <input type="number"
+                                    v-model="rank.discount_percent"
+                                    class="w-full h-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 shadow-sm shadow-muesli-300 px-5 text-center"
+                                    placeholder="Nhập giá trị giảm" />
                             </div>
                             <div class="flex justify-center gap-2">
-                                <button type="submit"
-                                    class="bg-muesli-400 hover:bg-muesli-600 text-white px-3 py-2 rounded-sm w-23">
+                                <button type="button"
+                                    class="bg-muesli-400 hover:bg-muesli-600 text-white px-3 py-2 rounded-sm" @click.prevent="handleCreateRank">
                                     Tạo
                                 </button>
-                                <button type="submit"
-                                    class="bg-muesli-400 hover:bg-muesli-600 text-white px-3 py-2 rounded-sm w-23">
+                                <button type="button"
+                                    class="bg-muesli-400 hover:bg-muesli-600 text-white px-3 py-2 rounded-sm" @click.prevent="handleUpdateRank">
                                     Cập Nhật
                                 </button>
-                                <button type="submit"
-                                    class="bg-muesli-400 hover:bg-muesli-600 text-white px-3 py-2 rounded-sm w-23">
+                                <button type="button"
+                                    class="bg-muesli-400 hover:bg-muesli-600 text-white px-3 py-2 rounded-sm" @click.prevent="handleDeleteRank">
+                                    Xoá
+                                </button>
+                                <button type="reset"
+                                    class="bg-muesli-400 hover:bg-muesli-600 text-white px-3 py-2 rounded-sm">
                                     Làm Mới
                                 </button>
                             </div>
                         </form>
                     </div>
                     <div class="w-2/3">
-                        <table class="w-full border border-gray-300 text-sm text-center bg-white">
+                        <table class="w-full border border-gray-300 text-sm text-center">
                             <thead class="bg-gradient-to-r from-muesli-200 to-muesli-400 text-white">
                                 <tr>
                                     <th class="px-4 py-2 border truncate max-w-[120px]">Hạng</th>
@@ -165,17 +164,17 @@
                                 </tr>
                             </thead>
                             <tbody class="text-gray-700">
-                                <tr class="hover:bg-muesli-100 transition odd:bg-white even:bg-gray-100" v-for="i in 10"
-                                    :key="i">
-                                    <td class="py-2">Giảm Giá Nè</td>
-                                    <td class="py-2">GIAMGIANE</td>
-                                    <td class="py-2">%</td>
+                                <tr class="hover:bg-muesli-100 transition odd:bg-white even:bg-gray-100" v-for="rank in ranks"
+                                    :key="rank.id">
+                                    <td class="py-2">{{ rank.nameRank }}</td>
+                                    <td class="py-2">{{ rank.minPointRequired }}</td>
+                                    <td class="py-2">{{ rank.discount_percent }}%</td>
                                     <td class="py-2 flex justify-center items-center gap-2 h-full">
                                         <button
                                             class="bg-white text-muesli-400 border border-muesli-400 hover:bg-muesli-400 hover:text-white py-[9px] px-3 rounded-lg">
                                             <LockKeyhole class="w-4 h-4" />
                                         </button>
-                                        <Button
+                                        <Button @click="getRankById(rank.id)"
                                             class="bg-white text-muesli-400 border border-muesli-400 hover:bg-muesli-400 hover:text-white">
                                             <SquarePen />
                                         </Button>
@@ -310,4 +309,32 @@ import {
     ChevronRight,
 } from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
+import { onMounted } from "vue";
+
+import { Rank } from '@/api/rank';
+const { rank, ranks, getAllRank, createRank, updateRank, deleteRank, isLoading } = Rank();
+
+const getRankById = async (id: number) => {
+    const selectRank = ranks.value.find((rank) => rank.id === id);
+    if(selectRank) rank.value = {...selectRank};
+}
+
+const handleCreateRank = async () => {
+    await createRank(rank.value);
+    await getAllRank();
+}
+
+const handleUpdateRank = async () => {
+    await updateRank(rank.value);
+    await getAllRank();
+}
+
+const handleDeleteRank = async () => {
+    await deleteRank(rank.value.id);
+    await getAllRank();
+}
+
+onMounted( async () => {
+    await getAllRank();
+})
 </script>
