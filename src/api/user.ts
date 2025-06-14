317f46a99852b36230f18e936fd959aabbc43ec0
Kiet <kietkid1st@gmail.com>
2025-06-14 21:02:08 +0700

    Fix Pull

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,66 +1,65 @@
-// import axios from "axios";
-// import { ref } from "vue";
-// import { toast } from 'vue-sonner'
+import axios from "axios";
+import { ref } from "vue";
+import { toast } from 'vue-sonner'
 
-// const baseUrl = import.meta.env.VITE_API_BASE_URL;
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-// interface Role {
-//   id: number;
-//   roleName: string;
-//   users: string[];
-// }
+interface Role {
+  id: number;
+  roleName: string;
+  users: string[];
+}
 
-// interface User {
-//   id: number;
-//   phone: string;
-//   email: string;
-//   password: string;
-//   gender: boolean;
-//   birthday: string;
-//   joinDate: string;
-//   updateDate: string;
-//   fullname: string;
-//   cccd: string;
-//   point: number;
-//   role: Role;
-//   eblacklist: 'FIRST' | 'SECOND' | 'THIRD' | 'NONE';
-// }
+interface User {
+  id: number;
+  phone: string;
+  email: string;
+  password: string;
+  gender: boolean;
+  birthday: string;
+  joinDate: string;
+  updateDate: string;
+  fullname: string;
+  cccd: string;
+  point: number;
+  role: Role;
+  eblacklist: 'FIRST' | 'SECOND' | 'THIRD' | 'NONE';
+}
 
-// const isLoading = ref(false);
+const isLoading = ref(false);
 
-// export const User = () => {
-//     const users = ref<User[]>([]);
-//     const user = ref<User>({
-//         id: 0,
-//         phone: '',
-//         email: '',
-//         password: '',
-//         gender: false,
-//         birthday: '',
-//         joinDate: '',
-//         updateDate: '',
-//         fullname: '',
-//         cccd: '',
-//         point: 0,
-//         role: {
-//             id: 0,
-//             roleName: '',
-//             users: []
-//         },
-//         eblacklist: 'NONE'
-//     });
+export const User = () => {
+    const users = ref<User[]>([]);
+    const user = ref<User>({
+        id: 0,
+        phone: '',
+        email: '',
+        password: '',
+        gender: false,
+        birthday: '',
+        joinDate: '',
+        updateDate: '',
+        fullname: '',
+        cccd: '',
+        point: 0,
+        role: {
+            id: 0,
+            roleName: '',
+            users: []
+        },
+        eblacklist: 'NONE'
+    });
 
-//     const getAllUsers = async () => {
-//         isLoading.value = true;
-//         try {
-//             const response = await axios.get(`${baseUrl}/users`);
-//             users.value = response.data;
-//         } catch (error) {
-//             toast.error("Lỗi khi lấy danh sách người dùng");
-//         } finally {
-//             isLoading.value = false;
-//         }
-//     };
+    const getUsersById = async (id: number) => {
+        try {
+            isLoading.value = true;
+            const response = await axios.get(`${baseUrl}/users/${id}`);
+            user.value = response.data;
+            isLoading.value = false;
+        } catch (error: any) {
+            toast.error(error.response.data.message);
+        }
+    }
 
-//     return { users, user, getAllUsers };
-// }
+    return { users, user, getUsersById, isLoading };
+}
