a5018ed29c75cb8e5174b2167777ce57583adbf3
Kiet <kietkid1st@gmail.com>
2025-07-06 12:53:19 +0700

    Sign In

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,58 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 import { ref } from "vue";
-import { toast } from 'vue-sonner'
+import { toast } from "vue-sonner";
+
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
 interface LoginResponse {
-    accessToken: string
+  accessToken: string;
 }
-export const Auth = () =>{
 
-const isLoading = ref(false)
-const login = async (username: string, password: string): Promise<LoginResponse> => {
-    isLoading.value = true 
-    try{
-        const response = await axios.post<LoginResponse>(`${baseUrl}/login`,{
-            username, 
-            password
-        })
-        console.log("status: "+response.status)
-        if(response.status === 200){
-            const token = response.data.accessToken
-            localStorage.setItem("acccessToken", token)
-            toast.success('Thông báo',{
-                description: 'Đăng nhập thành công' +response.status,
-                action: {
-                    label: 'Thoát',
+export interface User {
+  id: number;
+  phone: string | null;
+  email: string;
+  password: string | null;
+  gender: boolean | null;
+  birthday: string | null;
+  joinDate: string;
+  fullname: string;
+  cccd: string | null;
+  point: number;
+  role: Role;
+  rank: Rank;
+  eblacklist: number;
+}
 
-                },
-            })
-            setTimeout(() => {
-                window.location.href = '/administration/dashboard';
-            },1000);
-            return response.data
-        }else {
-            toast('Thông báo',{
-                description: 'Đăng nhập thất bại '+ response.status,
-                action: {
-                    label: 'Thoát',
-                    onClick: () => {}
-                },
-            })
-            return response.data
-        }
-    }catch(error){
-        toast.error('Thông báo',{
-                description: 'Lỗi không xác định ' + error,
-            })
-        throw error
-    }finally{
-        setTimeout(() => {
-            isLoading.value = false
-        },1000);
+export interface Role {
+  id: number;
+  roleName: string;
+}
+
+export interface Rank {
+  id: number;
+  nameRank: string;
+  minPointRequired: number;
+  discount_percent: number;
+}
+
+export const Auth = () => {
+  const isLoading = ref(false);
+
+  const getUserInfoByToken = async (): Promise<User> => {
+    try {
+      const response = await axios.get<User>(`${baseUrl}/me`, {
+        withCredentials: true,
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Lỗi lấy thông tin người dùng", error);
+      throw error;
     }
-}
-return {
+  };
+
+  const login = async (username: string, password: string): Promise<LoginResponse> => {
+    isLoading.value = true;
+
+    try {
+      const response = await axios.post<LoginResponse>(`${baseUrl}/login`, {
+        username,
+        password,
+      });
+      const token = response.data.accessToken;
+
+      // Gửi token qua cookie vì BE đọc từ cookie "jwt"
+      document.cookie = `jwt=${token}; path=/`;
+      //Lấy thông tin sau khi login
+      const user = await getUserInfoByToken();
+      localStorage.setItem("userInfo", JSON.stringify(user));
+      toast.success("Thông báo", {
+        description: "Đăng nhập thành công!",
+        action: { label: "Thoát" },
+      });
+
+      setTimeout(() => {
+        window.location.href = "/administration/dashboard";
+      }, 1000);
+
+      return response.data;
+    } catch (error: any) {
+      console.error("Đăng Nhập Thất Bại!", error);
+      toast.error("Thông báo", {
+        description: "Lỗi: " + (error?.response?.data?.message || error.message),
+      });
+      throw error;
+    } finally {
+      setTimeout(() => {
+        isLoading.value = false;
+      }, 1000);
+    }
+  };
+
+  return {
     login,
-    isLoading
+    isLoading,
+  };
 };
-}   
