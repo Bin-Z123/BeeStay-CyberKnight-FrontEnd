3d9643ea82e59c7d6d689061d73de15eb844ee95
Bin-Z123 <binvtps40171@gmail.com>
2025-07-14 11:58:31 +0700

    push

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -2,9 +2,7 @@
   <section>
     <div class="flex flex-col xl:flex-row">
       <div class="w-1/2"></div>
-      <div
-        class="xl:w-1/2 flex items-center h-screen mx-3 justify-center xl:justify-start"
-      >
+      <div class="xl:w-1/2 flex items-center h-screen mx-3 justify-center xl:justify-start">
         <div class="bg-white w-120 rounded-2xl p-10">
           <h1 class="text-muesli-400 font-bold text-2xl flex justify-center">
             Đăng Nhập
@@ -12,35 +10,18 @@
           <form action="">
             <div>
               <label class="text-muesli-400">Tài khoản</label>
-              <input
-                v-model="form.username"
-                type="text"
-                placeholder="Nhập email/sđt"
-                class="w-full h-12 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300"
-              />
+              <input v-model="form.username" type="text" placeholder="Nhập email/sđt"
+                class="w-full h-12 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300" />
             </div>
             <div>
               <label class="text-muesli-400">Mật khẩu</label>
-              <input
-                v-model="form.password"
-                type="password"
-                placeholder="Nhập mật khẩu"
-                class="w-full h-12 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300"
-              />
+              <input v-model="form.password" type="password" placeholder="Nhập mật khẩu"
+                class="w-full h-12 px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-muesli-200 mb-3 shadow-sm shadow-muesli-300" />
             </div>
             <div class="flex mb-4">
               <div class="w-1/2 flex items-center">
-                <input
-                  type="checkbox"
-                  name="remember"
-                  id="remember"
-                  class="h-4 w-4 rounded"
-                />
-                <label
-                  for="remember"
-                  class="ml-2 text-sm text-gray-500 select-none"
-                  >Nhớ mật khẩu</label
-                >
+                <input type="checkbox" name="remember" id="remember" class="h-4 w-4 rounded" />
+                <label for="remember" class="ml-2 text-sm text-gray-500 select-none">Nhớ mật khẩu</label>
               </div>
               <div class="w-1/2 text-right">
                 <div class="text-sm text-muesli-400 hover:underline">
@@ -49,25 +30,18 @@
               </div>
             </div>
             <div>
-              <button
-                :disabled="isLoading"
-                @click.prevent="handleLogin()"
-                class="bg-muesli-400 flex items-center justify-center hover:bg-white hover:text-muesli-400 border border-muesli-400 text-white font-bold py-2 px-4 w-full rounded duration-300 disabled:opacity-50 disabled:cursor-pointer disabled:hover:bg-muesli-400 disabled:hover:text-white"
-              >
-                <span v-if="isLoading"
-                  ><LoaderCircle class="animate-spin"
-                /></span>
+              <button :disabled="isLoading" @click.prevent="handleLogin()"
+                class="bg-muesli-400 flex items-center justify-center hover:bg-white hover:text-muesli-400 border border-muesli-400 text-white font-bold py-2 px-4 w-full rounded duration-300 disabled:opacity-50 disabled:cursor-pointer disabled:hover:bg-muesli-400 disabled:hover:text-white">
+                <span v-if="isLoading">
+                  <LoaderCircle class="animate-spin" />
+                </span>
                 <span v-else>Đăng Nhập</span>
               </button>
             </div>
             <div>
               <p class="text-center text-gray-500 mt-4">
                 Bạn chưa có tài khoản?
-                <RouterLink
-                  to="/auth/register"
-                  class="text-muesli-400 hover:underline"
-                  >Đăng ký</RouterLink
-                >
+                <RouterLink to="/auth/register" class="text-muesli-400 hover:underline">Đăng ký</RouterLink>
               </p>
             </div>
           </form>
