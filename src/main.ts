9b23d0929635804fb5b4563b6b03388f0bc89915
Bin-Z123 <binvtps40171@gmail.com>
2025-07-28 14:41:03 +0700

    add lib tanstack

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import { VueQueryPlugin, VueQueryPluginOptions } from "@tanstack/vue-query";
 import VueDatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
 import "keen-slider/keen-slider.min.css";
@@ -13,4 +14,9 @@ const app = createApp(App);
 app.component("VueDatePicker", VueDatePicker);
 app.use(createPinia());
 app.use(router);
+app.use(
+    VueQueryPlugin, {
+    enableDevtoolsV6Plugin: true
+}
+)
 app.mount("#app");
