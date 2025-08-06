65e16f6e35449915cc48859c5c7adfc26bdc428f
Bin-Z123 <binvtps40171@gmail.com>
2025-08-06 15:52:59 +0700

    UI chat

--- src/pages/administrantion/manager/ManagerNotificationPage.vue
+++ src/pages/administrantion/manager/ManagerNotificationPage.vue
@@ -1,3 +1,87 @@
 <template>
-    <h1>Notification</h1>
+    <div class="flex h-[59rem] overflow-hidden">
+
+        <!-- Main -->
+        <main class="flex-1 flex flex-col">
+            <!-- Nội dung chat -->
+            <div class="flex-1 overflow-y-auto p-4">
+                <h1 class="justify-self-center">Chat</h1>
+                <div v-for="(msg, i) in messages" :key="i" class="mb-2"><template v-if="msg.includes(':')">
+                        <span class="font-bold">{{ msg.slice(0, msg.indexOf(':')) }}</span>:<span>{{
+                            msg.slice(msg.indexOf(':') + 1) }}</span>
+                    </template>
+                    <template v-else>
+                        {{ msg }}
+                    </template>
+                </div>
+
+            </div>
+
+            <!-- Thanh chat (nằm dưới cùng vì margin-top: auto) -->
+            <div class="w-full bg-white border-t border-gray-200 p-4">
+                <label class="flex gap-2">
+                    <input v-model="message" @keyup.enter="sendMessage()" placeholder="Nhắn tin..."
+                        class="flex-1 h-10 px-4 border rounded" />
+                    <button class="px-4 bg-blue-500 text-white rounded" @click="sendMessage()">Gửi</button>
+                </label>
+            </div>
+        </main>
+    </div>
 </template>
+
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import SockJS from 'sockjs-client'
+import { Client } from '@stomp/stompjs'
+import { toast } from 'vue-sonner';
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+const message = ref('')
+const messages = ref([])
+let stompClient = null;
+const token = localStorage.getItem('acccessToken')
+const notification = ref([])
+
+const client = new Client({
+
+    webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+    connectHeaders: {
+        Authorization: `Bearer ${token}`,
+    },
+    reconnectDelay: 5000,
+    onConnect: () => {
+        stompClient = client
+        toast.success('Kết nối Socket thành công!')
+        message.value = 'Đã tham gia'
+        sendMessage()
+        client.subscribe('/topic/messages', (message) => {
+            messages.value.push(message.body)
+        })
+    },
+    onDisconnect: () => {
+        message.value = 'Đã rời khỏi'
+        sendMessage()
+    },
+    onStompError: (frame) => {
+        console.error('Lỗi Socket', frame)
+    },
+    // debug: (str) => console.log(str)
+})
+
+onMounted(() => {
+    client.activate()
+    console.log('Token:', token)
+})
+
+const sendMessage = () => {
+    if (message.value.trim() && stompClient) {
+        // stompClient.send('/app/chat', {}, message.value)
+        stompClient?.publish({
+            destination: '/app/chat',
+            body: message.value
+        });
+        message.value = ''
+    }
+}
+</script>
--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  define: {
+    global: "globalThis",
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
-    allowedHosts: [
-      'd3133e5081b2.ngrok-free.app',
-    ],
+    allowedHosts: ["d3133e5081b2.ngrok-free.app"],
   },
 });
